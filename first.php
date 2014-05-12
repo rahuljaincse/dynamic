68aa22b36d3bcb2eddd27260f3240d693a0ad530
rahuljaincse <jenumjaytishashnum>
2014-05-12 19:26:16 +0530

    updated

---
+++ first.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>TechITSoft</title>
+</head>
+
+<body>
+<form action = "first_code.php" method ="POST">
+<table align="center">
+<tr>
+<td>Enter First Value</td>
+<td><input type="text" name="txtf"/></td>
+</tr>
+<tr>
+<td>Enter Second Value</td>
+<td><input type="text" name="txts"/></td>
+</tr>
+<td colspan = "2" align ="center">
+<input type = "submit" value ="add" />
+</td>
+</tr></table></form>
+
+
+
+
+
+
+</body>
+</html>
---
+++ second.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>TechITSoft</title>
+</head>
+
+<body><form action ="second_code.php" method="POST">
+<table align="center">
+<tr>
+<td>enter radius of circle</td>
+<td> <input type = "text" name ="txtf"> </td>
+</tr>
+<td colspan ="2" align =" center ">
+<input type ="submit" value="Area"/>
+</td>
+</tr>
+</table></form>
+
+
+</body>
+</html>
